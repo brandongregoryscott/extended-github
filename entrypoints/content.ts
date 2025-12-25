86e4eb5773cb14e10a6edf12ca1c3333e9cf59df
Brandon Scott <contact@brandonscott.me>
2025-12-25 14:31:49 -0500

    feat: logic to assign self to own PRs via assign yourself button or popover, logic to short-circuit if already assigned

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -1,6 +1,60 @@
-export default defineContentScript({
-  matches: ['*://*.google.com/*'],
-  main() {
-    console.log('Hello content.');
+import { sleep } from "@/utilities/core-utils";
+import {
+  assignSelfViaPopover,
+  findAssignYourselfButton,
+  getAuthenticatedUserName,
+  getPullRequestAuthorUserName,
+  isAuthenticatedUserAssigned,
+  toggleAssigneesPopover,
+} from "@/utilities/dom-utils";
+
+const PULL_REQUEST_URL_PATTERN = "*://github.com/*/*/pull/*";
+const watchPattern = new MatchPattern(PULL_REQUEST_URL_PATTERN);
+
+async function checkPullRequestsPage() {
+  const authenticatedUser = getAuthenticatedUserName();
+  const pullRequestAuthor = getPullRequestAuthorUserName();
+  if (
+    authenticatedUser == null ||
+    pullRequestAuthor == null ||
+    authenticatedUser !== pullRequestAuthor
+  ) {
+    return;
+  }
+
+  const isAssigned = isAuthenticatedUserAssigned();
+  if (isAssigned) {
+    return;
+  }
+
+  const assignYourselfButton = findAssignYourselfButton();
+  if (assignYourselfButton != null) {
+    assignYourselfButton.click();
+    return;
+  }
+
+  toggleAssigneesPopover();
+  await sleep(250);
+  toggleAssigneesPopover();
+  await sleep(250);
+  toggleAssigneesPopover();
+  await sleep(1000);
+  assignSelfViaPopover();
+  await sleep(500);
+  toggleAssigneesPopover();
+}
+
+const contentScript = defineContentScript({
+  matches: [PULL_REQUEST_URL_PATTERN],
+  async main(ctx) {
+    await checkPullRequestsPage();
+
+    ctx.addEventListener(window, "wxt:locationchange", async ({ newUrl }) => {
+      if (watchPattern.includes(newUrl)) {
+        await checkPullRequestsPage();
+      }
+    });
   },
 });
+
+export default contentScript;
---
+++ utilities/dom-utils.ts
@@ -0,0 +1,126 @@
+const ElementType = {
+  Anchor: "a",
+  Button: "button",
+  Meta: "meta",
+  Label: "label",
+} as const;
+
+const AttributeName = {
+  Name: "name",
+  DataMenuTrigger: "data-menu-trigger",
+} as const;
+
+const AttributeValue = {
+  AssigneesSelectMenu: "assignees-select-menu",
+  UserLogin: "user-login",
+} as const;
+
+const ElementText = {
+  AssignYourself: "assign yourself",
+} as const;
+
+const ClassName = {
+  Author: "author",
+  Assignee: "assignee",
+  AssigneeListItemUsername: "js-username",
+};
+
+function findElementByInnerText<T extends HTMLElement = HTMLElement>(
+  type: keyof HTMLElementTagNameMap,
+  innerText: string
+): T | undefined {
+  const elements = Array.from(document.querySelectorAll<T>(type));
+  return elements.find((element) => element.innerText === innerText);
+}
+
+function findAssigneesPopoverTrigger(): HTMLElement | undefined {
+  return (
+    document.querySelector<HTMLElement>(
+      `[${AttributeName.DataMenuTrigger}="${AttributeValue.AssigneesSelectMenu}"]`
+    ) ?? undefined
+  );
+}
+
+function getAuthenticatedUserName(): string | undefined {
+  const authenticatedUserMeta = document.querySelector(
+    `${ElementType.Meta}[${AttributeName.Name}="${AttributeValue.UserLogin}"]`
+  );
+
+  if (authenticatedUserMeta == null) {
+    return undefined;
+  }
+
+  return (authenticatedUserMeta as any).content;
+}
+
+function getPullRequestAuthorUserName(): string | undefined {
+  const authorLink = document.querySelector<HTMLElement>(
+    `${ElementType.Anchor}.${ClassName.Author}`
+  );
+  if (authorLink == null) {
+    return undefined;
+  }
+
+  return authorLink.innerText;
+}
+
+function isAuthenticatedUserAssigned(): boolean {
+  const authenticatedUserName = getAuthenticatedUserName();
+  const assignees = Array.from(
+    document.querySelectorAll<HTMLElement>(
+      `${ElementType.Anchor}.${ClassName.Assignee}`
+    )
+  );
+
+  return (
+    assignees.find(
+      (assignee) => assignee.innerText === authenticatedUserName
+    ) != null
+  );
+}
+
+function findAssignYourselfButton(): HTMLButtonElement | undefined {
+  return findElementByInnerText(ElementType.Button, ElementText.AssignYourself);
+}
+
+function findAuthenticatedAssigneeListItem(): HTMLElement | undefined {
+  const username = getAuthenticatedUserName();
+  if (username == null) {
+    return undefined;
+  }
+
+  const listItems = Array.from(
+    document.querySelectorAll<HTMLElement>(
+      `.${ClassName.AssigneeListItemUsername}`
+    )
+  );
+  return (
+    listItems
+      .find((element) => element.innerText === username)
+      ?.closest<HTMLElement>(ElementType.Label) ?? undefined
+  );
+}
+
+function toggleAssigneesPopover(): void {
+  const assigneesPopover = findAssigneesPopoverTrigger();
+  assigneesPopover?.click();
+}
+
+function assignSelfViaPopover(): void {
+  const authenticatedUserListItem = findAuthenticatedAssigneeListItem();
+  if (authenticatedUserListItem?.ariaChecked === true.toString()) {
+    return;
+  }
+
+  authenticatedUserListItem?.click();
+}
+
+export {
+  isAuthenticatedUserAssigned,
+  getAuthenticatedUserName,
+  getPullRequestAuthorUserName,
+  findAssignYourselfButton,
+  toggleAssigneesPopover,
+  assignSelfViaPopover,
+  findElementByInnerText,
+};
