bc423fe49f7bda27762941b0d33e11c0e125a24a
Brandon Scott <contact@brandonscott.me>
2025-12-25 21:16:20 -0500

    chore: update package name/description/icon (#2)

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ["@wxt-dev/module-react"],
-  manifest: { name: "Extended GitHub", permissions: ["storage"] },
+  modules: ["@wxt-dev/module-react", "@wxt-dev/auto-icons"],
+  manifest: {
+    name: "Extended GitHub",
+    description: "Browser extension with additional GitHub functionality",
+    permissions: ["storage"],
+  },
 });
