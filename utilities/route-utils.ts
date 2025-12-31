c3ff1e4a2843c745e740ccfc9470429e0f635616
Brandon Scott <contact@brandonscott.me>
2025-12-30 20:23:37 -0500

    refactor: move url patterns to RouteUtils class, add logic for checking query strings

--- utilities/route-utils.ts
+++ utilities/route-utils.ts
@@ -5,6 +5,18 @@ type PullRequestPath = {
 };
 
 class RouteUtils {
+    static readonly EXISTING_PULL_REQUEST_URL_PATTERN =
+        "*://github.com/*/*/pull/*";
+    // Note: These patterns do NOT match on query strings, so that check needs to be performed manually.
+    static readonly NEW_PULL_REQUEST_URL_PATTERN =
+        "*://github.com/*/*/compare/*...*";
+    static readonly EXISTING_PULL_REQUEST_MATCH_PATTERN = new MatchPattern(
+        this.EXISTING_PULL_REQUEST_URL_PATTERN
+    );
+    static readonly NEW_PULL_REQUEST_MATCH_PATTERN = new MatchPattern(
+        this.NEW_PULL_REQUEST_URL_PATTERN
+    );
+
     static parsePullRequestPath(pathname: string): PullRequestPath {
         const [_empty, organization, repo, _pull, pullRequestNumberAsString] =
             pathname.split("/");
@@ -13,6 +25,22 @@ class RouteUtils {
             pullRequestNumber: parseInt(pullRequestNumberAsString),
             repo,
         };
+    }
+
+    static matchesNewPullRequestUrl(
+        url: string | URL = window.location.href
+    ): boolean {
+        const searchParams = new URLSearchParams(window.location.search);
+        return (
+            this.NEW_PULL_REQUEST_MATCH_PATTERN.includes(url) &&
+            searchParams.get("expand") === "1"
+        );
+    }
+
+    static matchesExistingPullRequestUrl(
+        url: string | URL = window.location.href
+    ): boolean {
+        return this.EXISTING_PULL_REQUEST_MATCH_PATTERN.includes(url);
     }
 }
 
